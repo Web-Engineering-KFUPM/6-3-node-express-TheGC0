1bcb3545ebdaeab012a8c6f1a5b2cb1999be4578
AliShamah <ali_shamah@outlook.com>
2026-04-06 12:06:28 +0300

    refactor: organize route definitions and import getRandomQuote function

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 const PORT = 3000;
@@ -8,14 +9,15 @@ const PORT = 3000;
 app.use(cors());
 app.use(morgan("dev"));
 
-
-// TODO 5: Import and apply Morgan middleware
-
-
-// TODO 6.1: Create root route "/"
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
 
 
-// TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
 // TODO 7: Start server using app.listen
