34fe8852ec75c67e4b16530e6105e1516deca08f
AliShamah <ali_shamah@outlook.com>
2026-04-06 12:05:30 +0300

    import and apply Morgan middleware for logging requests

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(morgan("dev"));
 
 
 // TODO 5: Import and apply Morgan middleware
